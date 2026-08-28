737acd39fd42ee00ff83cf01542c3cbba150656c
ajay kale <ajaykale749810@gmail.com>
2026-08-28 23:03:01 +0530

    Perfom String Operations

---
+++ StringOperations/Program.cs
@@ -0,0 +1,16 @@
+﻿namespace String;
+
+public class String
+{
+    public static void Main()
+    {
+        string Name = "  ajay  ";
+
+        Console.WriteLine("StringLenth:- "+Name.Length);
+        Console.WriteLine("UpperCase:- "+Name.ToUpper());
+        Console.WriteLine("LowerCase:- "+Name.ToLower());
+        Console.WriteLine("Trim:- "+Name.Trim());
+        Console.WriteLine("Contains a:- "+Name.Contains("a"));
+        Console.WriteLine("StartWith a:- "+Name.StartsWith("a"));
+    }
+}
