3cb5422f6fad96ab29af2b04c0fa29c3a3ab9113
ajay kale <ajaykale749810@gmail.com>
2026-09-11 09:37:43 +0530

    fix the code

--- Delegate/obj/Debug/net10.0/Delegate.AssemblyInfo.cs
+++ Delegate/obj/Debug/net10.0/Delegate.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Delegate")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3388b18c1fc987588debbf37a4b08f7a37528ba2")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+59616795471ac36681c51e988b2aec576d9aabbb")]
 [assembly: System.Reflection.AssemblyProductAttribute("Delegate")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Delegate")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
