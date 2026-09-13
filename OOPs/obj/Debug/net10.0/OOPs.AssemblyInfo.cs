f1001d13b745b428890eac3a0a8ba4cb93adcc48
ajay kale <ajaykale749810@gmail.com>
2026-09-14 00:13:47 +0530

    fix the code

--- OOPs/obj/Debug/net10.0/OOPs.AssemblyInfo.cs
+++ OOPs/obj/Debug/net10.0/OOPs.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("OOPs")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e3926ed08ac67c8252eb66c740a59b7ac091a21c")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c4f3df08deb772bf4aa73603f0fd8c112d5c3be8")]
 [assembly: System.Reflection.AssemblyProductAttribute("OOPs")]
 [assembly: System.Reflection.AssemblyTitleAttribute("OOPs")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
